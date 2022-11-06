dae8949aa5ea979e72d2e2da9ddd16a32e79ce4b
G <gianmarco.david@gmail.com>
2022-11-06 20:25:22 +0100

    Advent of code 2021 04

---
+++ 2021/04.hs
@@ -0,0 +1,69 @@
+import Data.List (transpose)
+
+type Board = [[Cell]]
+
+data Cell = Cell {number :: Int, marked :: Bool} deriving (Show)
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn d = loop []
+  where
+    loop acc [] = reverse acc
+    loop acc l =
+      let (before, after) = span (/= d) l
+       in loop (before : acc) (drop 1 after)
+
+parseExtractions :: String -> [Int]
+parseExtractions = map read . splitOn ','
+
+isWinning :: Board -> Bool
+isWinning rows = any isWinningLine (rows ++ cols)
+  where
+    cols = transpose rows
+    isWinningLine = all marked
+
+mark :: Int -> Board -> Board
+mark e = map (map markCell)
+  where
+    markCell (Cell n m) = Cell n (m || e == n)
+
+unmarkedSum :: Board -> Int
+unmarkedSum = sum . map (sum . map value)
+  where
+    value (Cell n m) = if m then 0 else n
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf n = loop []
+  where
+    loop acc [] = reverse acc
+    loop acc l =
+      let (h, t) = splitAt n l
+       in loop (h : acc) t
+
+parseBoards :: [String] -> [Board]
+parseBoards = chunksOf 5 . chunksOf 5 . map ((`Cell` False) . read) . concatMap words
+
+parseInput :: String -> ([Int], [Board])
+parseInput input = (parseExtractions firstLine, boards)
+  where
+    (firstLine : rest) = lines input
+    boards = parseBoards . filter (not . null) $ rest
+
+solve1 :: [Int] -> [Board] -> Int
+solve1 (e : es) boards =
+  let marked = map (mark e) boards
+   in case filter isWinning marked of
+        (wb : _) -> unmarkedSum wb * e
+        [] -> solve1 es marked
+
+solve2 :: [Int] -> [Board] -> Int
+solve2 extractions boards = loop extractions boards 0
+  where
+    loop (e : es) boards score =
+      let marked = map (mark e) boards
+       in case filter isWinning marked of
+            (_ : _) -> loop es (filter (not . isWinning) marked) (unmarkedSum (last marked) * e)
+            [] -> loop es marked score
+    loop [] _ score = score
+
+solveFile :: ([Int] -> [Board] -> Int) -> FilePath -> IO ()
+solveFile solve fileName = print . uncurry solve . parseInput =<< readFile fileName
